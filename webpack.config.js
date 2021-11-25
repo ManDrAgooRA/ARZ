57f1564fb0ed2cb9e68a3099badac5e319f2fe36
Eugene Gorobey <eugene.gorobey@nixsolutions.com>
2021-11-25 13:53:43 +0200

    ARZ-01 Set up project

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HTMLWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: ['@babel/polyfill', './src/index.jsx'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].js'
+    },
+    devServer: {
+        port: 3000,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new HTMLWebPackPlugin({ template: './src/index.html' }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env']
+                    }
+                }
+            },
+        ]
+    }
+}
