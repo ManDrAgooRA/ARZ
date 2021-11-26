0e14a9e2adbc278a77fcd6774f9897ce97716dca
Eugeniy <myEmail@example.com>
2021-11-26 17:26:23 +0200

    fix error

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,4 @@ import { render } from "react-dom";
 import App from "./components/App";
 import "./style.scss";
 
-render(
-  <App />,
-  document.getElementById(
-    "root"
-  )
-);
+render(<App />, document.getElementById("root"));
