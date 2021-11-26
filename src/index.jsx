f1e3f94b33f8064761216972dbd3cbf997020452
Eugeniy <myEmail@example.com>
2021-11-26 18:24:39 +0200

    add husky and prettier

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./style.scss";
 
-// <<<<<<< HEAD
-// render(<App />, document.getElementById("root"));
-// =======
 ReactDOM.render(<App />, document.getElementById("root"));
-// >>>>>>> 2eda55edd99a90d89204579e0382937b86743b36
