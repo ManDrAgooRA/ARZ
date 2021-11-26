64431a787b56f264b6b3fbef5005d7e203872985
Eugeniy <myEmail@example.com>
2021-11-26 10:56:30 +0200

    change gitingore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import '../style.scss';
 
-function App() {
-  return (
-    <div className="test-block">
-      <h1>React text22</h1>
-    </div>
-  );
-}
-
-// const App = function () {
+// function App() {
 //   return (
 //     <div className="test-block">
 //       <h1>React text22</h1>
 //     </div>
 //   );
-// };
+// }
+
+const App = () => (
+  <div className="test-block">
+    <h1>React text24</h1>
+  </div>
+);
 
 export default App;
