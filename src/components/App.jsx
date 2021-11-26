bce614b2732b700c51e63afda831a7655eb34c88
Eugeniy <myEmail@example.com>
2021-11-26 10:36:52 +0200

    change eslint config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../style.scss';
 
-export default function foo() {
+function App() {
   return (
     <div className="test-block">
-      <h1>React text2</h1>
+      <h1>React text22</h1>
     </div>
   );
 }
+
+// const App = function () {
+//   return (
+//     <div className="test-block">
+//       <h1>React text22</h1>
+//     </div>
+//   );
+// };
+
+export default App;
