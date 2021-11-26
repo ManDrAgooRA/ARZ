7345e0900a936f3a256617a6d59c9539136450c0
Eugeniy <myEmail@example.com>
2021-11-26 17:44:00 +0200

    add husky

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./style.scss";
 
+// <<<<<<< HEAD
+// render(<App />, document.getElementById("root"));
+// =======
 ReactDOM.render(<App />, document.getElementById("root"));
-
+// >>>>>>> 2eda55edd99a90d89204579e0382937b86743b36
