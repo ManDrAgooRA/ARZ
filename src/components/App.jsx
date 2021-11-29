bae80b362b245b3d7112688b58c90f6590360978
Eugeniy <myEmail@example.com>
2021-11-29 16:42:23 +0200

    change eslint config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import "../style.scss";
+import React from 'react';
+import '../style.scss';
 
 const App = () => (
   <div className="test-block">
     <h1>React text24</h1>
+    <div className="box">
+      <span>1</span>
+    </div>
   </div>
 );
 
