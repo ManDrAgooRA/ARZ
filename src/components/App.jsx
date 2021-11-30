c2174a5dd58ad06fa48472e7dc378f323ef9b4d7
Eugeniy <myEmail@example.com>
2021-11-30 11:01:35 +0200

    create header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
+import Header from './Header';
 import '../style.scss';
 
-const App = () => (
-  <div className="test-block">
-    <h1>React text24</h1>
-    <div className="box">
-      <span>1</span>
-    </div>
-  </div>
-);
+const App = () => {
+  return <Header />;
+};
 
 export default App;
