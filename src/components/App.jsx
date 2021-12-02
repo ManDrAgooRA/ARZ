a0522050b3740d78b414f1a252d36ca9730bd7d0
Eugene <gorobey@bubuntu-lx.nixsolutions.com>
2021-12-02 13:10:44 +0200

    add Layout component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Grommet } from 'grommet';
 import { THEME } from '../constants';
-import AppBar from './AppBar/AppBar';
-import Footer from './Footer/Footer';
+// import AppBar from './AppBar/AppBar';
+// import Footer from './Footer/Footer';
 import Sidebar from './Sidebar/Sidebar';
+import Layout from './Layout/Layout';
 import './styles/style.scss';
 
 const App = () => {
   return (
     <Grommet theme={THEME}>
-      <AppBar />
-      <Sidebar />
-      <Footer />
+      <Layout>
+        <Sidebar />
+      </Layout>
     </Grommet>
   );
 };
