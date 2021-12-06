c0dcb31444bfee894dbd2ad094bbf22c087e6cef
Eugene <gorobey@bubuntu-lx.nixsolutions.com>
2021-12-06 11:13:14 +0200

    fix error

--- src/BusinessLogic/index.js
+++ src/BusinessLogic/index.js
@@ -1,7 +1,7 @@
 import { fetchData } from '../Api/get';
 
 export const getGoods = async () => {
-  const fetchGoods = await fetchData().json();
+  const fetchGoods = await fetchData();
   const parseGoods = await fetchGoods.json();
 
   return parseGoods;
