07d2c03cdffff85db6ab8e5bc93c586d32d7b7f6
Eugene <gorobey@bubuntu-lx.nixsolutions.com>
2021-12-07 18:40:28 +0200

    clear routes

--- src/routes/allRoutes.js
+++
@@ -1,7 +0,0 @@
-import SinglePage from '../pages/SinglePage/SinglePage';
-import Error from '../pages/Error/Error';
-
-export const allRoutes = [
-  { path: '/singlePage', component: SinglePage },
-  { path: '*', component: Error },
-];
