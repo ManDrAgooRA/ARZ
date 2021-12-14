83584378fc7ab70750db8e809c54b54c19df3dae
Eugene <gorobey@bubuntu-lx.nixsolutions.com>
2021-12-14 11:06:30 +0200

    add validation search form, moved range selector to a single component

--- src/components/Filters/FilterByPrice/FilterByPrice.jsx
+++ src/components/Filters/FilterByPrice/FilterByPrice.jsx
@@ -1,92 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Box, Collapsible, RangeSelector, Stack, Text } from 'grommet';
+import React, { useState } from 'react';
+import { Box, Collapsible } from 'grommet';
 import { CaretLeftFill } from 'grommet-icons';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import { setMinPrice, setMaxPrice } from '../../../store/actions';
-import { getMinValue, getMaxValue } from '../../../utils';
+import MyRangeSelector from '../../UI/RangeSelector/RangeSelector';
 import './filterByPrice.scss';
-
-const schema = yup
-  .object({
-    minPrice: yup.number().required(),
-    maxPrice: yup.number().required(),
-  })
-  .required();
-
-const Thin = () => {
-  const { goods, minPrice, maxPrice } = useSelector((state) => state.goods);
-  const dispatch = useDispatch();
-  const [range, setRange] = useState([
-    getMinValue({ goods }),
-    getMaxValue({ goods }),
-  ]);
-
-  const {
-    register,
-    handleSubmit,
-    // formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  const changeValue = () => {
-    dispatch(setMinPrice(range[0]));
-    dispatch(setMaxPrice(range[1]));
-  };
-
-  const onSubmit = () => {
-    changeValue();
-  };
-
-  useEffect(() => {
-    dispatch(setMinPrice(+range[0]));
-    dispatch(setMaxPrice(+range[1]));
-  }, []);
-
-  return (
-    <>
-      <Stack>
-        <div className="price-range">
-          <RangeSelector
-            direction="horizontal"
-            min={minPrice}
-            max={maxPrice}
-            step={1}
-            values={range}
-            onChange={(nextRange) => {
-              setRange(nextRange);
-            }}
-          />
-        </div>
-      </Stack>
-      <Box align="center">
-        <Text size="small">{`${range[0]}₴ - ${range[1]}₴`}</Text>
-        <div className="price-filter">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            From:
-            <input
-              {...register('minValue')}
-              value={range[0]}
-              onChange={(e) => setRange([+e.target.value, range[1]])}
-            />
-            To:
-            <input
-              {...register('maxValue')}
-              value={range[1]}
-              onChange={(e) => setRange([range[0], +e.target.value])}
-            />
-            <button type="submit" className="btn">
-              submit
-            </button>
-          </form>
-        </div>
-      </Box>
-    </>
-  );
-};
 
 const FilterByPrice = () => {
   const [open, setOpen] = useState(false);
@@ -106,7 +22,7 @@ const FilterByPrice = () => {
         </button>
       </Box>
       <Collapsible open={open}>
-        <Thin label="My Range Selector" />
+        <MyRangeSelector label="My Range Selector" />
       </Collapsible>
     </>
   );
---
+++ src/components/UI/RangeSelector/RangeSelector.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Box, RangeSelector, Stack, Text } from 'grommet';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { setMinPrice, setMaxPrice } from '../../../store/actions';
+import { getMinValue, getMaxValue } from '../../../utils';
+import './rangeSelector.scss';
+
+const schema = yup.object().shape({
+  minValue: yup.number().positive().integer(),
+  maxValue: yup.number().positive().integer(),
+});
+
+const MyRangeSelector = () => {
+  const { goods, minPrice, maxPrice } = useSelector((state) => state.goods);
+  const dispatch = useDispatch();
+  const [range, setRange] = useState([
+    getMinValue({ goods }),
+    getMaxValue({ goods }),
+  ]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      minValue: getMinValue({ goods }),
+      maxValue: getMaxValue({ goods }),
+    },
+  });
+
+  const changeValue = () => {
+    dispatch(setMinPrice(range[0]));
+    dispatch(setMaxPrice(range[1]));
+  };
+
+  const onSubmit = () => {
+    changeValue();
+  };
+
+  useEffect(() => {
+    dispatch(setMinPrice(+range[0]));
+    dispatch(setMaxPrice(+range[1]));
+  }, []);
+
+  return (
+    <>
+      <Stack>
+        <div className="price-range">
+          <RangeSelector
+            direction="horizontal"
+            min={minPrice}
+            max={maxPrice}
+            step={1}
+            values={range}
+            onChange={(nextRange) => {
+              setRange(nextRange);
+            }}
+          />
+        </div>
+      </Stack>
+
+      <Box align="center">
+        <Text size="small">{`${range[0]}₴ - ${range[1]}₴`}</Text>
+        <div className="price-filter">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            From:
+            <input
+              {...register('minValue')}
+              type="number"
+              value={range[0]}
+              error={errors?.minValue?.message}
+              onChange={(e) => setRange([+e.target.value, range[1]])}
+            />
+            To:
+            <input
+              {...register('maxValue')}
+              type="number"
+              value={range[1]}
+              onChange={(e) => setRange([range[0], +e.target.value])}
+            />
+            <input type="submit" value="Ok" className="btn" />
+          </form>
+          {errors.minValue && (
+            <p className="error">{errors.minValue.message}</p>
+          )}
+          {errors.maxValue && (
+            <p className="error">{errors.maxValue.message}</p>
+          )}
+        </div>
+      </Box>
+    </>
+  );
+};
+
+export default MyRangeSelector;
