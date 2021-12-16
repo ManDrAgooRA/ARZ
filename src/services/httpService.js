de8bf9b3bbc3622779e33df257a7fd639c9dd577
Eugene <gorobey@bubuntu-lx.nixsolutions.com>
2021-12-16 17:43:13 +0200

    add private request

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,9 @@
 export const baseUrl = (path) => {
   return `${process.env.API_KEY}${path}`;
 };
-
 export default class HTTPService {
   static get(path) {
+    console.log(`${baseUrl(path)}`);
     return fetch(`${baseUrl(path)}`)
       .then((response) => {
         return response;
