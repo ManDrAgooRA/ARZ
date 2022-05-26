89047cd8e3620b463a2cfdfae55cfd54a4053bb1
zordan <zekagorobey@gmail.com>
2022-05-26 16:26:26 +0300

    setup env in server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
 
-const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
 const dotenv = require('dotenv');
 
 dotenv.config();
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
 
 const middlewares = jsonServer.defaults({
   static: './build',
